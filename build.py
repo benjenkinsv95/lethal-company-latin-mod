c51b226fe4e1285676f92e4764458eb933803299
Ben Jenkins <benjenkinsv95@gmail.com>
2023-12-09 21:07:51 -0600

    Update build to produce macron and macronless versions

--- build.py
+++ build.py
@@ -14,25 +14,29 @@ def remove_macrons(text):
     }
     return ''.join(macron_map.get(c, c) for c in text)
 
-def process_specific_files_and_folders():
-    """Process specific files and folders in the current directory and create a zip file 'Lethal Company Latin.zip'."""
+def process_specific_files_and_folders_with_two_zips():
+    """Process specific files and folders in the current directory and create two zip files."""
     current_dir = os.getcwd()
-    zip_name = 'Lethal Company Latin.zip'
+    zip_name_normal = 'Lethal Company Latin.zip'
+    zip_name_macron = 'Lethal Company Latin Macron.zip'
     files_and_folders_to_include = ['BepInEx', 'CHANGELOG.md', 'icon.png', 'manifest.json', 'README.md']
 
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # Copy specified files and folders
+    with tempfile.TemporaryDirectory() as temp_dir_normal, tempfile.TemporaryDirectory() as temp_dir_macron:
+        # Copy specified files and folders to both temporary directories
         for item in files_and_folders_to_include:
             src_path = os.path.join(current_dir, item)
             if os.path.exists(src_path):
-                dst_path = os.path.join(temp_dir, item)
+                dst_path_normal = os.path.join(temp_dir_normal, item)
+                dst_path_macron = os.path.join(temp_dir_macron, item)
                 if os.path.isdir(src_path):
-                    shutil.copytree(src_path, dst_path)
+                    shutil.copytree(src_path, dst_path_normal)
+                    shutil.copytree(src_path, dst_path_macron)
                 else:
-                    shutil.copy2(src_path, dst_path)
+                    shutil.copy2(src_path, dst_path_normal)
+                    shutil.copy2(src_path, dst_path_macron)
 
-        # Process files in the temporary directory
-        for root, dirs, files in os.walk(temp_dir):
+        # Process files in the temporary directory for normal zip (remove macrons)
+        for root, dirs, files in os.walk(temp_dir_normal):
             for file in files:
                 if file.endswith(('.txt', '.cfg')):
                     file_path = os.path.join(root, file)
@@ -42,14 +46,21 @@ def process_specific_files_and_folders():
                     with open(file_path, 'w', encoding='utf-8') as f:
                         f.write(content)
 
-        # Create zip file
-        with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, dirs, files in os.walk(temp_dir):
+        # Create zip file without macrons
+        with zipfile.ZipFile(zip_name_normal, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, dirs, files in os.walk(temp_dir_normal):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    zipf.write(file_path, os.path.relpath(file_path, start=temp_dir))
+                    zipf.write(file_path, os.path.relpath(file_path, start=temp_dir_normal))
 
-    return zip_name
+        # Create zip file with macrons (original content)
+        with zipfile.ZipFile(zip_name_macron, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, dirs, files in os.walk(temp_dir_macron):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    zipf.write(file_path, os.path.relpath(file_path, start=temp_dir_macron))
+
+    return zip_name_normal, zip_name_macron
 
 # Example usage
-process_specific_files_and_folders()
+process_specific_files_and_folders_with_two_zips()
